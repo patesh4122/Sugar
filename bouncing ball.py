c251b71e5751219bbfbbd6cfd2d837c6c5ad4ad4
shivam <94002848+patesh4122@users.noreply.github.com>
2022-04-05 11:43:57 -0400

    Add files via upload

---
+++ bouncing ball.py
@@ -0,0 +1,4 @@
+first_number = int(input("What is the first number you want to add?"))
+secound_number = int(input("What is the second number you want to add?"))
+sum = first_number + secound_number
+print("The sum of your two number is", sum)
