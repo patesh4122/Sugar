0415c6a6b92bab6ca5b831a2f755dac29f0535da
shivam <94002848+patesh4122@users.noreply.github.com>
2022-04-05 11:52:04 -0400

    Delete addmatchine.py

--- addmatchine.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        first_number = int(input("What is the first number you want to add?"))
-        secound_number = int(input("What is the second number you want to add?"))
-        sum = first_number + secound_number
-        print("The sum of your two number is", sum)
-        break
-    except:
-        print("You are dumb. That is not an integer...")
